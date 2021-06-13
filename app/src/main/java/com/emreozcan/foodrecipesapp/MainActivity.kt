885f991c764a8bc21a3d1c89b94f1113c5b52e4e
Emre Ozcan <eozcan97@gmail.com>
2021-06-13 13:33:43 +0300

    Bottom Navigation Completed

--- app/src/main/java/com/emreozcan/foodrecipesapp/MainActivity.kt
+++ app/src/main/java/com/emreozcan/foodrecipesapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.emreozcan.foodrecipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.emreozcan.foodrecipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.homeFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokeFragment
+        ))
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
